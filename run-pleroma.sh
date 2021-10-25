a912e79e53d0a59df64ea0252b312dc69d053dde
AndreVuillemot160 <84594011+AndreVuillemot160@users.noreply.github.com>
2021-10-25 10:46:00 +0300

    Create run-pleroma.sh

---
+++ run-pleroma.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "-- Waiting for database..."
+while ! pg_isready -U "${POSTGRES_USER:-pleroma}" -d "postgres://${POSTGRES_HOST:-postgres}:5432/${POSTGRES_DB:-pleroma}" -t 1; do
+  echo "Waiting for ${POSTGRES_HOST-postgres} to come up..." >&2
+  sleep 1s
+done
+
+pleroma_ctl migrate
+
+if [ "${USE_RUM:-n}" = "y" ] ; then
+  pleroma_ctl migrate --migrations-path priv/repo/optional_migrations/rum_indexing/
+fi
+
+if [ "${USE_SOAPBOX:-n}" = "y" ]; then
+  unzip -o /tmp/soapbox-fe.zip -d /var/lib/pleroma
+  rm /tmp/soapbox-fe.zip
+fi
+
+exec pleroma start
