bec3764d53f685bcff8928d78370407a8b2c783b
Oscar <silkky@silkky.cloud>
2021-11-30 17:06:53 +1100

    Fix

--- config.exs
+++ config.exs
@@ -3,35 +3,6 @@ import Config
 
 config :pleroma, configurable_from_database: true
 
-# Welcome message
-config :pleroma, :welcome,
-  email: [
-    enabled: true,
-    sender: {"Pleroma Silkky.Cloud", "noreply@robot.silkky.cloud"},
-    subject: "Welcome to <%= instance_name %>!"
-  ]
-
-# MRF policies
-config :pleroma, :mrf_simple,
-  media_nsfw: [
-    {"*.pawoo.net"},
-    {"*.baraag.net"}
-  ],
-  federated_timeline_removal: [
-    {"gameliberty.club"},
-    {"youjo.love", "Illegal content"},
-    {"hentai.baby", "Illegal content"}
-  ],
-  reject: [
-    {"eunomia.social", "Privacy Concerns"},
-    {"*.eunomia.social", "Privacy Concerns"},
-    {"childpawn.shop", "Illegal content"},
-    {"freak.university", "Illegal content"},
-    {"wintermute.fr.to", "Illegal content"},
-    {"hentai.baby", "Illegal content"},
-    {"youjo.love", "Illegal content"}
-  ]
-
 # Media proxy
 config :pleroma, :media_proxy,
   enabled: true
