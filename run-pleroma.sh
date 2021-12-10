77589e7b07f38f2b9818786827b2ddb6b90ccf0a
Your local trollman <cia@example.org>
2021-12-10 22:45:34 +0300

    Bring back some old stuff...

--- run-pleroma.sh
+++ run-pleroma.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+if [ ! -e "$PLEROMA_CONFIG_PATH" ] ; then
+  gen-config.sh
+fi
+
 echo "-- Waiting for database..."
 while ! pg_isready -U "${POSTGRES_USER:-pleroma}" -d "postgres://${POSTGRES_HOST:-postgres}:5432/${POSTGRES_DB:-pleroma}" -t 1; do
   echo "Waiting for ${POSTGRES_HOST:-postgres} to come up..." >&2
